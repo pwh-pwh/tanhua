bc37d3a755a12d0d2af980714d46606dba803ff6
coderpwh <plmoknpwh@163.com>
2022-07-20 17:30:02 +0800

    参加测试代码

--- tanhua-server/src/test/kotlin/org/coderpwh/server/TestToday.kt
+++ tanhua-server/src/test/kotlin/org/coderpwh/server/TestToday.kt
@@ -1,6 +1,8 @@
 package org.coderpwh.server
 
+import org.coderpwh.server.service.TodayBestService
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 /**
@@ -11,12 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class TestToday {
     //eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibW9iaWxlIjoiMTc2MDIwMjY4NjgifQ.AgcbMhqF0llgfcUffjrpAVPjwL-OWwwOibUHZZ_7nbI
-
+    @Autowired
+    lateinit var todayBestService: TodayBestService
     @Test
     fun testTd() {
-        /*var todayBest =
+        var todayBest =
             todayBestService.queryTodayBest("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibW9iaWxlIjoiMTc2MDIwMjY4NjgifQ.AgcbMhqF0llgfcUffjrpAVPjwL-OWwwOibUHZZ_7nbI")
-        println(todayBest)*/
+        println(todayBest)
         println("hello")
 
     }
